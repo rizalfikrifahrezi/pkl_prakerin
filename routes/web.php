409c6db27e0fa5026bbb8fe971b7a105d33113d1
rizalfikrifahrezi <rizalviqrifahrezi74925@gmail.com>
2019-08-09 16:53:05 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(['register' => false]);
+
+Route::get('home','HomeController@index')->name('home');
+Route::group(['prefix' => 'admin','middleware'=>['auth','role:superadmin']], function(){
+    Route::get('/', function() {
+        return 'hallo';
+    });
+    Route::resource('user','UserController');
+});
